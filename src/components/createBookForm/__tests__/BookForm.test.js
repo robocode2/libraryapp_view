8a12db874e2fbc58852ed685408bb886b8eb24ba
robocode2 <rabie.abbas@code.berlin>
2022-02-01 16:56:01 +0100

    Wrote a few tests for BookForm

--- src/components/createBookForm/__tests__/BookForm.test.js
+++ src/components/createBookForm/__tests__/BookForm.test.js
@@ -3,40 +3,103 @@ import {
   screen,
   render,
   fireEvent,
+  cleanup,
   getByPlaceholderText,
 } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
 import { act } from "react-dom/test-utils";
 import "@testing-library/jest-dom/extend-expect";
 import BookForm from "../BookForm";
 
 describe("testing form functionality", () => {
+  //here is my first test
   test("if form title value is changed", () => {
-    const mockHandleSubmit = jest.fn();
     const component = render(<BookForm />);
-    //const input = component.container.querySelector("Entertitle");
     const form = component.container.querySelector("form");
     //const titleNode = screen.getByPlaceholderText("Enter title");
     const titleNode = screen.getByLabelText("title");
-    const inputNode = screen.getByPlaceholderText("Enter title");
-    /*  await act(async () => {
-    fireEvent.change(inputNode, {
-      target: { value: "testing of forms could be easier" },
-    });
-  }); */
+    //const inputNode = screen.getByPlaceholderText("Enter title");
+
     fireEvent.change(titleNode, {
       target: { value: "testing of forms could be easier" },
     });
-    /*  await act(async () => {
-    fireEvent.submit(form);
-  }); */
-    //
-    expect(titleNode).toHaveDisplayValue("testing of forms could be easier");
-    //expect(mockHandleSubmit.mock.calls).toHaveLength(1);
-    //expect(inputNode).toHaveTextContent("testing of forms could be easier");
-    //expect(postToAPI.mock.calls[0][0].content).toBe(
-    // "testing of forms could be easier"
-    // );
+
+    expect(titleNode.value).toMatch("testing of forms could be easier");
+  });
+
+  test("if form description value is changed", async () => {
+    const component = render(<BookForm />);
+    const descriptionNode = screen.getByPlaceholderText("description");
+
+    await act(async () => {
+      fireEvent.change(descriptionNode, {
+        target: { value: "little women cookies" },
+      });
+    });
+    expect(descriptionNode.value).toMatch("little women cookies");
+  });
+
+  test("if form isbn value is changed", () => {
+    const { getByPlaceholderText } = render(<BookForm />);
+    const isbnNode = screen.getByPlaceholderText("Enter isbn");
+    fireEvent.change(isbnNode, {
+      target: { value: "12312421412421" },
+    });
+    expect(isbnNode.value).toMatch("12312421412421");
+  });
+
+  test("if Submit is clicked and HandleSubmit is launched", async () => {
+    const mockHandleSubmit = jest
+      .fn()
+      .mockImplementation((cb) => () => cb({ test: "test" }));
+
+    const component = render(<BookForm submit={mockHandleSubmit} />);
+    const form = component.container.querySelector("form");
+    const titleNode = screen.getByLabelText("title");
+
+    fireEvent.change(titleNode, {
+      target: { value: "testing of forms could be easier" },
+    });
+
+    await act(async () => {
+      fireEvent.submit(form);
+    });
+
+    expect(mockHandleSubmit.mock.calls).toHaveLength(1);
+  });
+
+  test("if all values are entered and form submitted", async () => {
+    const mockNotify = jest.fn();
+
+    const component = render(<BookForm submit={mockNotify} />);
+    const form = component.container.querySelector("form");
+
+    const titleNode = screen.getByLabelText("title");
+    const descriptionNode = screen.getByPlaceholderText("description");
+    const isbnNode = screen.getByPlaceholderText("Enter isbn");
+
+    fireEvent.change(titleNode, {
+      target: { value: "Little Women" },
+    });
+    fireEvent.change(isbnNode, {
+      target: { value: "123124125" },
+    });
+    fireEvent.change(descriptionNode, {
+      target: { value: "Little Women strolling round" },
+    });
+
+    await act(async () => {
+      fireEvent.submit(form);
+    });
+
+    expect(mockNotify.mock.calls).toHaveLength(1);
+
+    expect(isbnNode.value).toMatch("123124125");
+    expect(titleNode.value).toMatch("Little Women");
+    expect(descriptionNode.value).toMatch("Little Women strolling round");
   });
 });
+afterEach(cleanup);
